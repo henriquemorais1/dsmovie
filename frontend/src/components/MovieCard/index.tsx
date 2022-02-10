7a1cbc3dba29ca57e17dec94ec39fd91fc1a29c0
henriquemorais1 <99363989+henriquemorais1@users.noreply.github.com>
2022-02-10 10:22:54 -0300

    teste2

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -12,9 +12,11 @@ function MovieCard() {
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div>
+            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore count={movie.count} score={movie.score}/>
+                <MovieScore />
 
 
                 <Link to={`/form/${movie.id}`}>
