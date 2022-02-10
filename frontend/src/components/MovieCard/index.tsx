17e420febb4dd37e67662abe01567096fa06518b
henriquemorais1 <99363989+henriquemorais1@users.noreply.github.com>
2022-02-10 10:39:58 -0300

    teste final

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -17,7 +17,7 @@ function MovieCard() {
                 <MovieScore />
                            
 
-                  <Link to={`/form/${movie.id}`}>
+                <Link to={'/form/${movie.id}'}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
             </div>
